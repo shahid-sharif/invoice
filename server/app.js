1b7d30470664ee58bd812f256881029259100986
mr-fahad-03 <mdfahadzulfiqar@gmail.com>
2026-01-10 16:51:32 +0500

    ****

--- server/app.js
+++ server/app.js
@@ -56,8 +56,18 @@ const upload = multer({
   },
 })
 
-// Middleware
-app.use(cors())
+// Middleware - Configure CORS properly
+app.use(cors({
+  origin: [
+    "https://invoice-builder-madina-shop.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost:8080"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
 app.use(express.json())
 
 // MongoDB connection
